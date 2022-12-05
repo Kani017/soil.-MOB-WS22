5f9e15df4be52f14f399d007661ba9f3f9212de3
[Kani] <[thies.boll@gmail.com]>
2022-12-05 18:42:48 +0100

    overflown pixel bug fix

--- soil-master/lib/pages/login_page.dart
+++ soil-master/lib/pages/login_page.dart
@@ -48,128 +48,112 @@ class _LoginPageState extends State<LoginPage> {
           ),
 
           child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
 
-              //logo image
-              Image(
-                image: AssetImage("images/icon2.png"),
-              ),
-              SizedBox(height: 10),
-              // "soil." title
-              Image(
-                image: AssetImage("images/soil.png"),
-              ),
-
-              SizedBox(height: 15),
-              Text(
-                'Your plantcare NILS TEST solution',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 17,
-                 color: Color(0xFF1F312B)
+                //logo image
+                Image(
+                  image: AssetImage("images/icon2.png"),
                 ),
-              ),
-              SizedBox(height: 130),
+                SizedBox(height: 10),
+                // "soil." title
+                Image(
+                  image: AssetImage("images/soil.png"),
+                ),
 
-              //Email Textfield
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xFFAFCEBF),
-                    border: Border.all(color: Color(0xFF1F312B)),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: TextField(
-                      style: TextStyle(fontSize: 17),
-                      controller: _emailController,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Email',
-                        )
-                    ),
+                SizedBox(height: 15),
+                Text(
+                  'Your plantcare solution',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                   color: Color(0xFF1F312B)
                   ),
                 ),
-              ),
-              SizedBox(height: 10),
+                SizedBox(height: 130),
 
-              //password section
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xFFAFCEBF),
-                    border: Border.all(color: Color(0xFF1F312B)),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: TextField(
-                      style: TextStyle(fontSize: 17),
-                      controller: _passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Password',
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-
-              //Sign in button
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: GestureDetector(
-                  onTap: signIn,
+                //Email Textfield
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
                   child: Container(
-                    padding: EdgeInsets.all(20),
                     decoration: BoxDecoration(
-                      color: Color(0xFF24362D),
+                      color: Color(0xFFAFCEBF),
+                      border: Border.all(color: Color(0xFF1F312B)),
                       borderRadius: BorderRadius.circular(12),
                     ),
-                    child: Center(
-                      child: Text(
-                        'Sign In',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        style: TextStyle(fontSize: 17),
+                        controller: _emailController,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Email',
+                          )
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+
+                //password section
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFAFCEBF),
+                      border: Border.all(color: Color(0xFF1F312B)),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        style: TextStyle(fontSize: 17),
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Password',
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 25),
-              //not a member?
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Not a member?',
-                    style: TextStyle(
-                      shadows: <Shadow>[
-                        Shadow(
-                          offset: Offset(2.0, 2.0),
-                          blurRadius: 50.0,
-                          color: Colors.black,
+                SizedBox(height: 10),
+
+                //Sign in button
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: GestureDetector(
+                    onTap: signIn,
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF24362D),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
                         ),
-                      ],
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                      ),
                     ),
                   ),
-                  GestureDetector(
-                    onTap: widget.showRegisterPage,
-                    child: Text(
-                      ' Register now!',
+                ),
+                SizedBox(height: 25),
+                //not a member?
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Not a member?',
                       style: TextStyle(
                         shadows: <Shadow>[
                           Shadow(
@@ -178,17 +162,35 @@ class _LoginPageState extends State<LoginPage> {
                             color: Colors.black,
                           ),
                         ],
-                        fontSize: 18,
-                        color: Colors.cyan,
-
+                        fontSize: 16,
                         fontWeight: FontWeight.bold,
+                        color: Colors.white,
                       ),
                     ),
-                  ),
-                  SizedBox(height: 140),
-                ],
-              ),
-            ],
+                    GestureDetector(
+                      onTap: widget.showRegisterPage,
+                      child: Text(
+                        ' Register now!',
+                        style: TextStyle(
+                          shadows: <Shadow>[
+                            Shadow(
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 50.0,
+                              color: Colors.black,
+                            ),
+                          ],
+                          fontSize: 18,
+                          color: Colors.cyan,
+
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 140),
+                  ],
+                ),
+              ],
+            ),
           ),
         )
       ),
